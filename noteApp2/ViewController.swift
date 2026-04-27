2be474d97419ada6a3cb50c26d04218ed308b653
Mustafa Ghazi <ghazimostafa10@icloud.com>
2026-04-27 14:49:21 +0200

    init push

---
+++ noteApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  noteApp2
+//
+//  Created by Mustafa Ghazi on 27/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
